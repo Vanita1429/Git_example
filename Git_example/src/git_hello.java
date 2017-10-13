2603cf53be0b7356141697ef05d6c23f75436e6e
VanitaShashank <VanitaShashank@192.168.1.16>
2017-10-12 21:34:55 -0700

    This is my second commit (Bingo)

--- Git_example/src/git_hello.java
+++ Git_example/src/git_hello.java
@@ -3,6 +3,8 @@ public class git_hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");}
+		System.out.println("Hello World");
+		System.out.println("Bingo");
+				}
 
 }
